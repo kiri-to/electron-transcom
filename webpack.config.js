92bce08351243308d2691c8c3016e40d1a2fbd54
kiri-to <1311437486@qq.com>
2024-01-25 15:26:53 +0800

    upadate projecr structure

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,35 @@ const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: process.env.NODE_ENV,
+  // mode: "production",
+  mode: "development",
   entry: './src/renderer.js',
   output: {
     filename: 'renderer.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  // resolve: {
+  //   alias: {           //alias配置作用于导入语句如import，此处不生效
+  //     'wasm': path.resolve(__dirname, 'node_modules/scichart/_wasm/'),
+  //   }
+  // }, 
   plugins: [
     new CopyPlugin({
-        patterns: [
-            { from: "src/index.html", to: "" },
-            { from: "src/index.css", to: "" },
-            { from: "src/main.js", to: "" },
-            { from: "src/preload.js", to: "" },
-            { from: "node_modules/scichart/_wasm/scichart2d.data", to: "" },
-            { from: "node_modules/scichart/_wasm/scichart2d.wasm", to: "" },
-          ],
+      patterns: [
+        { from: "src/index.html", to: "" },
+        { from: "src/main.js", to: "" },
+        { from: "src/preload.js", to: "" },
+        { from: "node_modules/scichart/_wasm/scichart2d.data", to: "" },
+        { from: "node_modules/scichart/_wasm/scichart2d.wasm", to: "" },
+      ],
     })
   ]
 };
